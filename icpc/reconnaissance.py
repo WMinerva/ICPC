5ae709bb8b6e018017357f1d3e52b7735c3181ce
WMinerva <nicolas.ochoa1697@gmail.com>
2024-06-25 11:53:31 -0400

    icpc py

---
+++ icpc/reconnaissance.py
@@ -0,0 +1,15 @@
+n = int(input())
+soldados = list(map(int, input().split()))
+
+min = abs(soldados[-1] - soldados[0])
+
+for i in range(n - 1):
+    if abs(soldados[i] - soldados[i + 1]) < min:
+        min = abs(soldados[i] - soldados[i + 1])
+
+for i in range(n - 1):
+    if abs(soldados[i] - soldados[i + 1]) == min:
+        print(i + 1, i + 2)
+        break
+else:
+    print(n, 1)
